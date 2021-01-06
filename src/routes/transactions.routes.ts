fb0ce1ff0e2f32e82d2bcfb4c3d0e3828f8fd888
antonylrds <42950733+antonylrds@users.noreply.github.com>
2021-01-05 23:55:20 -0300

    Categorias incluídas na listagem das transações

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -10,7 +10,18 @@ const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
   const transactionsRepository = getCustomRepository(TransactionsRepository);
-  const transactions = await transactionsRepository.find();
+  const transactions = await transactionsRepository.find({
+    select: [
+      'id',
+      'title',
+      'value',
+      'type',
+      'category',
+      'created_at',
+      'updated_at',
+    ],
+    relations: ['category'],
+  });
   const balance = await transactionsRepository.getBalance();
 
   return response.json({ transactions, balance });
