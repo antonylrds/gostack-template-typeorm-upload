0a13e67f4409d4b81ed8fc915c0ed93c0d134cb1
antonylrds <42950733+antonylrds@users.noreply.github.com>
2021-01-05 21:59:01 -0300

    Decorators implementados e Migrations criadas

---
+++ src/database/migrations/1609893010169-CreateTransactionsTable.ts
@@ -0,0 +1,67 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class CreateTransactionsTable1609893010169
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'transactions',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'category_id',
+            type: 'uuid',
+            isNullable: true,
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'type',
+            type: 'enum',
+            enum: ['income', 'outcome'],
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+          },
+        ],
+      }),
+    );
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        name: 'TransactionCategory',
+        columnNames: ['category_id'],
+        referencedTableName: 'categories',
+        referencedColumnNames: ['id'],
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('transactions', 'TransactionCategory');
+    await queryRunner.dropTable('transactions');
+  }
+}
--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,39 @@
+import {
+  Entity,
+  Column,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+} from 'typeorm';
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn()
   id: string;
 
+  @Column()
   title: string;
 
+  @Column('enum')
   type: 'income' | 'outcome';
 
+  @Column('number')
   value: number;
 
+  @Column()
   category_id: string;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
