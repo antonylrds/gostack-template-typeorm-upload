4f221f25a844275936d5a9ce87357cadbe0baa90
antonylrds <42950733+antonylrds@users.noreply.github.com>
2021-01-05 22:08:30 -0300

    getBalance implementado

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,40 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    let income = 0;
+    let outcome = 0;
+
+    if (transactions.length > 0) {
+      const incomes = transactions.map(transaction => {
+        if (transaction.type === 'income') {
+          return transaction.value;
+        }
+        return 0.0;
+      });
+
+      const outcomes = transactions.map(transaction => {
+        if (transaction.type === 'outcome') {
+          return transaction.value;
+        }
+        return 0.0;
+      });
+
+      income = incomes.reduce(
+        (accumulator, currentValue) => accumulator + currentValue,
+      );
+
+      outcome = outcomes.reduce(
+        (accumulator, currentValue) => accumulator + currentValue,
+      );
+    }
+
+    return {
+      income,
+      outcome,
+      total: income - outcome,
+    };
   }
 }
 
